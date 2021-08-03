bd769af4c63b4a75335ab11f923f19a89f9a0b09
Triana Cerda <trianaRcerda@gmail.com>
2021-08-02 19:09:56 -0700

    rendering functions and needs styling

--- src/App.js
+++ src/App.js
@@ -9,7 +9,6 @@ class App extends Component {
       <>  
         <Header />
         <Recipe />
-        {/* <Author /> */}
         <Footer />
       </>
     )
--- src/Recipe/Recipe.js
+++ src/Recipe/Recipe.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import './Recipe.css';
 import chocoPizza from '../assets/choco-pizza.png';
 import printIcon from '../assets/print-icon.png';
-import listBg from '../assets/list-bg.png';
+
 
 class Recipe extends Component {
     render() { 
@@ -29,7 +29,6 @@ class Recipe extends Component {
                     </p>
 
                     <div className='ingredients'>
-                        <img className='list-img' src={listBg} alt='list'/>
                         <IngredientList/>
                     </div>
                 </section>
@@ -62,51 +61,51 @@ const ingredientItem = [
         name: 'pink salt',
         amount: '1/2 tsp',
     },
-    // {
-    //     id: 4, 
-    //     name: '',
-    //     amount: '',
-    // },
-    // {
-    //     id: 5, 
-    //     name: '',
-    //     amount: '',
-    // },
-    // {
-    //     id: 6, 
-    //     name: '',
-    //     amount: '',
-    // },
-    // {
-    //     id: 7, 
-    //     name: '',
-    //     amount: '',
-    // },
-    // {
-    //     id: 8, 
-    //     name: '',
-    //     amount: '',
-    // },
-    // {
-    //     id: 9, 
-    //     name: '',
-    //     amount: '',
-    // },
-    // {
-    //     id: 10, 
-    //     name: '',
-    //     amount: '',
-    // },
-    // {
-    //     id: 11, 
-    //     name: '',
-    //     amount: '',
-    // },
-    // {
-    //     id: 12, 
-    //     name: '',
-    //     amount: '',
-    // },
+    {
+        id: 4, 
+        name: 'Black Mission Figs',
+        amount: '1 lb',
+    },
+    {
+        id: 5, 
+        name: 'brown sugar',
+        amount: '1/2 cup',
+    },
+    {
+        id: 6, 
+        name: 'water',
+        amount: '2-4 tbsp',
+    },
+    {
+        id: 7, 
+        name: 'heavy cream',
+        amount: '1 1/2',
+    },
+    {
+        id: 8, 
+        name: 'granulated sugar',
+        amount: '1/3',
+    },
+    {
+        id: 9, 
+        name: 'egg yolks',
+        amount: '2',
+    },
+    {
+        id: 10, 
+        name: 'lemon,juiced',
+        amount: '1',
+    },
+    {
+        id: 11, 
+        name: 'butter',
+        amount: '2 tbsp',
+    },
+    {
+        id: 12, 
+        name: 'honey roasted pevans, roughly chopped',
+        amount: '1 cup',
+    },
 ]
 
 class IngredientList extends Component {
